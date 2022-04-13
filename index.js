65d18eb99b73bfcaa29da2ff79d447e926f8bbb5
FelipeBarroso <felipebarroso299@gmail.com>
2022-04-13 02:50:30 -0300

    Utilizando o gitignore

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const { connect } = require("http2");
+const { password } = require("pg/lib/defaults");
 
 const Client = require("pg").Client;
 const cliente = new Client({
-  user: "postgres",
-  password: "FelipeB@1",
-  host: "127.0.0.1",
-  port: 5432,
-  database: "Carros",
+  user: process.env.user,
+  password: process.env.password,
+  host: process.env.localhost,
+  port: process.env.port,
+  database: process.env.database,
 });
 
 //cliente.connect();
@@ -16,7 +17,7 @@ cliente
     const resultado = results.rows;
     console.table(resultado);
   })
-  .finally(() => cliente.end());
+  //.finally(() => cliente.end());
 
 getCarros();
 //insCarros("Hyudai", "HB20")
@@ -27,11 +28,11 @@ async function getCarros() {
     await cliente.connect();
     console.log("Conexão bem sucedidada!");
     const resultado = await cliente.query("select * from carros");
-    console.log(resultado.rows);
+    //console.table(resultado.rows);
   } catch (ex) {
     console.log("Ocorreu um erro no getCarros. Erro:" + ex);
   } finally {
-    await cliente.end();
+    //await cliente.end();
     //console.log("Cliente desconectado");
   }
 }
