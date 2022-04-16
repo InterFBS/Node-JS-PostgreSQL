2017ca1898e4556ee1d6653840575b6fd3d75631
FelipeBarroso <felipebarroso299@gmail.com>
2022-04-16 18:36:35 -0300

    Ajustando para deletar do Banco de Dados

--- index.js
+++ index.js
@@ -1,28 +1,29 @@
 const { connect } = require("http2");
 const { password } = require("pg/lib/defaults");
 
+require("dotenv").config();
+
 const Client = require("pg").Client;
 const cliente = new Client({
-  user: process.env.user,
-  password: process.env.password,
-  host: process.env.localhost,
-  port: process.env.port,
-  database: process.env.database,
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  host: process.env.LOCALHOST,
+  port: process.env.PORT,
+  database: process.env.DATABASE,
 });
 
 //cliente.connect();
-cliente
-  .query("select * from carros")
-  .then((results) => {
-    const resultado = results.rows;
-    console.table(resultado);
-  })
-  //.finally(() => cliente.end());
+cliente.query("select * from carros").then((results) => {
+  const resultado = results.rows;
+  console.table(resultado);
+});
+//.finally(() => cliente.end());
 
-getCarros();
+//getCarros();
 //insCarros("Hyudai", "HB20")
+delCarro();
 
-async function getCarros() {
+/*async function getCarros() {
   try {
     console.log("Iniciando a conexão");
     await cliente.connect();
@@ -35,13 +36,13 @@ async function getCarros() {
     //await cliente.end();
     //console.log("Cliente desconectado");
   }
-}
+}*/
 
 /*async function insCarros(marca, modelo) {
   try {
     console.log("Iniciando a conexão");
     await cliente.connect();
-    console.log("Conexão bem sucedidada!");
+    console.log("Conexão bem sucedida!");
     await cliente.query('insert into carros("marca", "modelo") values('+"'" +marca +"', '" +modelo +"');");
     console.log("Valor inserido na tabela");
 
@@ -57,3 +58,23 @@ async function getCarros() {
   }
 }
   */
+
+async function delCarro(id) {
+  try {
+    console.log("Iniciando a conexão");
+    cliente.connect();
+    console.log("Conexão bem sucedida!");
+    await cliente.query(`DELETE FROM Carros WHERE id = ${id};`);
+    console.log("Carro deletado " + id);
+    console.log("Item removido da tabela");
+
+    const resultado = await cliente.query("select * from carros");
+    console.table(resultado.rows);
+  }
+    catch {
+    } 
+    finally {
+      await cliente.end();
+      console.log("Cliente desconectado");
+    }
+}
